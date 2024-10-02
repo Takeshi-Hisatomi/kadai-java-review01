75d51a586444740ca302979947091e91148e7b4b
takeshi.hisatomi <take_tomy7@yahoo.co.jp>
2024-10-02 22:25:05 +0900

    kadai-java-review01

--- src/Review01.java
+++ src/Review01.java
@@ -3,22 +3,14 @@ public class Review01 {
 
     public static void main(String[] args) {
         int price = 1500;
-        double tax = 1.1;
-        int result1;
-        int result2;
-        result1 = taxMethod1(price, tax);
-        result2 = taxMethod2(result1, price);
-        System.out.println(price + "円の商品の税込価格は" + result1 + "円（消費税は" + result2 + "円）です。");
+        double tax = 0.1;
+        int result = tax(price, tax);
+        System.out.println(price + "円の商品の税込価格は" + (price + result) + "円（消費税は" + result + "円）です。");
     }
-    //税込価格の計算
-    public static int taxMethod1(int price, double tax) {
-        double pit1 = price * tax;
-        return (int)pit1;
-    }
-    //消費税の計算
-    public static int taxMethod2(int result1, int price) {
-        int pit2 = result1 - price;
-        return pit2;
+    //消費税額の計算
+    public static int tax(int price, double tax) {
+        double pit = price * tax;
+        return (int)pit;
     }
 
 }
